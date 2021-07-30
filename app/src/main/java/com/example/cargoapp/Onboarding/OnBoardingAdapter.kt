5ddf9117e3699ba0168042de35d896834b8c2f41
Kaan Sarıkaya <45636360+kkaansrky@users.noreply.github.com>
2021-07-30 13:21:22 +0300

    initial and onboarding commit

---
+++ app/src/main/java/com/example/cargoapp/Onboarding/OnBoardingAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.cargoapp
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.cargoapp.Onboarding.FirstOnBoardingFragment
+import com.example.cargoapp.Onboarding.SecondOnBoardingFragment
+
+private const val FRAGMENT_COUNT = 2
+
+class OnboardingAdapter(requireActivity: FragmentActivity) : FragmentStateAdapter(requireActivity) {
+
+
+    override fun getItemCount(): Int = FRAGMENT_COUNT
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> FirstOnBoardingFragment()
+            1 -> SecondOnBoardingFragment()
+            else -> FirstOnBoardingFragment()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/cargoapp/Onboarding/OnBoardingFragment.kt
@@ -0,0 +1,55 @@
+package com.example.cargoapp.Onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.cargoapp.OnboardingAdapter
+import com.example.cargoapp.R
+import com.example.cargoapp.databinding.FragmentOnBoardingBinding
+
+
+class OnBoardingFragment : Fragment() {
+    private var _binding: FragmentOnBoardingBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        mainFunc()
+
+        return view
+    }
+
+    private fun mainFunc() {
+        val adapter = OnboardingAdapter(requireActivity())
+
+        binding.viewPager2.adapter = adapter
+        binding.wormDots.setViewPager2(binding.viewPager2)
+
+
+        binding.cardViewButton.setOnClickListener {
+
+            //Eğer son onboarding sayfasına geldiyse diye kontrol ediyoruz.
+            if (binding.viewPager2.currentItem == adapter.itemCount-1) {
+                findNavController().navigate(R.id.action_onBoardingFragment_to_mainFragment)
+            } else {
+                binding.viewPager2.currentItem = binding.viewPager2.currentItem + 1
+            }
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
