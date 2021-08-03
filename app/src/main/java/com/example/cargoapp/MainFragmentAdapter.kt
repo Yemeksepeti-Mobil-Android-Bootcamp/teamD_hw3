4e6be14aca9e8d2d1862ac9601bf6ca2dbb03bee
oktayagca <okty_agca@hotmail.com>
2021-08-03 14:36:22 +0300

    Bottom Navigation v1

--- app/src/main/java/com/example/cargoapp/MainFragmentAdapter.kt
+++ app/src/main/java/com/example/cargoapp/MainFragmentAdapter.kt
@@ -5,20 +5,28 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.cargoapp.ListFragment.CargoListFragment
 import com.example.cargoapp.Onboarding.FirstOnBoardingFragment
+import com.example.cargoapp.databinding.FragmentMainBinding
 
 
 private const val FRAGMENT_COUNT = 2
 
 class MainFragmentAdapter(requireActivity: FragmentActivity) : FragmentStateAdapter(requireActivity) {
-
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun getItemCount(): Int = FRAGMENT_COUNT
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> CargoListFragment()
-            1 -> CargoAddFragment()
-            else -> FirstOnBoardingFragment()
+            0 ->{
+                CargoListFragment()
+            }
+            1 ->{
+                CargoAddFragment()
+            }
+            else -> {
+                FirstOnBoardingFragment()
+            }
         }
     }
 
